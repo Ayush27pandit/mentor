93f58a42b2921e087b2032db61a677f390e39766
Siddhant Chakraborty <siddhantchakraborty977@gmail.com>
2024-08-21 00:09:39 +0530

    first

---
+++ mentor/src/App.tsx
@@ -0,0 +1,15 @@
+import Btn from "./components/Btn"
+import "./index.css"
+
+function App() {
+  
+  return (
+    <>
+<div className="bg-black text-white min-h-screen">
+    <Btn/>
+</div>
+         </>
+  )
+}
+
+export default App
---
+++ mentor/src/components/Btn.tsx
@@ -0,0 +1,14 @@
+import BlurIn from "./magicui/blur-in"
+import ShimmerButton from "./magicui/shimmer-button"
+
+const Btn = () => {
+  return (
+    <>
+    <BlurIn word="Hellow World"></BlurIn>
+    <ShimmerButton  className="">hii</ShimmerButton>
+    </>
+    
+  )
+}
+
+export default Btn
